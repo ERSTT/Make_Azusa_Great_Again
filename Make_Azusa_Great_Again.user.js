0f885f4feddcd6521fe94becb37b1edce498a341
ERST <158469810+ERSTT@users.noreply.github.com>
2025-01-19 22:16:52 +0800

    Update Make_Azusa_Great_Again.user.js

--- Make_Azusa_Great_Again.user.js
+++ Make_Azusa_Great_Again.user.js
@@ -2,7 +2,7 @@
 // @name         Make Azusa Great Again
 // @namespace    https://github.com/ERSTT
 // @icon         https://azusa.wiki/favicon.ico
-// @version      1.0
+// @version      1.1
 // @description  Make Azusa Great Again
 // @author       ERST
 // @match        https://azusa.wiki/*
@@ -19,7 +19,9 @@
 // @grant        GM_addStyle
 // @grant        GM_setValue
 // @grant        GM_getValue
-// @run-at       document-idle
+// @grant        GM_deleteValue
+// @grant        GM_listValues
+// @run-at       document-end
 // @changelog    初始发布
 // ==/UserScript==
 
@@ -32,43 +34,66 @@
 
     const jsonConfigUrl = 'https://raw.githubusercontent.com/ERSTT/Make_Azusa_Great_Again/refs/heads/main/data.json';
     let modulesConfig = [];
+    const scriptVersion = GM_info.script.version;
 
     function fetchModulesConfig() {
         GM_xmlhttpRequest({
             method: 'GET',
             url: jsonConfigUrl,
             onload: function(response) {
-                try {
-                    const newModulesConfig = JSON.parse(response.responseText);
-                    if (JSON.stringify(newModulesConfig) !== JSON.stringify(modulesConfig)) {
-                        modulesConfig = newModulesConfig;
-                        createControlPanel();
-                        updateControlPanel();
-                        loadAndExecuteModules();
-                    }
-                } catch (e) {}
-            },
-            onerror: function() {}
+                const config = JSON.parse(response.responseText);
+                const newModulesConfig = config.menu;
+                const remoteMagaVersion = config['maga-version'];
+
+                if (JSON.stringify(newModulesConfig) !== JSON.stringify(modulesConfig)) {
+                    modulesConfig = newModulesConfig;
+                    createControlPanel();
+                    updateControlPanel();
+                    loadAndExecuteModules();
+                }
+
+                if (remoteMagaVersion && remoteMagaVersion !== scriptVersion) {
+                    alertNewVersion(remoteMagaVersion);
+                }
+            }
         });
     }
 
+    function alertNewVersion(remoteMagaVersion) {
+        const newVersionAlert = document.getElementById('new-version-alert');
+
+        const remoteVersionNumber = parseFloat(remoteMagaVersion);
+        const scriptVersionNumber = parseFloat(scriptVersion);
+
+        if (remoteVersionNumber !== scriptVersionNumber) {
+            if (newVersionAlert) {
+                newVersionAlert.style.display = 'block';
+                newVersionAlert.innerText = `检测到MAGA新版本, 最新版本为 ${remoteMagaVersion}`;
+            }
+        } else {
+            if (newVersionAlert) {
+                newVersionAlert.style.display = 'none';
+            }
+        }
+    }
+
     function loadAndExecuteModules() {
-        modulesConfig.menu.forEach((menu, menuIndex) => {
-            menu.modules.forEach((module, index) => {
-                if (getModuleState(menuIndex, index)) {
-                    loadAndExecuteModule(menuIndex, index);
+        modulesConfig.forEach((menu) => {
+            menu.modules.forEach((module) => {
+                if (getModuleState(menu.id, module.id)) {
+                    loadAndExecuteModule(menu.id, module.id);
                 }
             });
         });
     }
 
-    function loadAndExecuteModule(menuIndex, index) {
-        const module = getModuleByIndex(menuIndex, index);
-        const localVersion = getModuleLocalVersion(menuIndex, index);
+    function loadAndExecuteModule(menuId, moduleId) {
+        const module = getModuleById(menuId, moduleId);
+        const localVersion = getModuleLocalVersion(menuId, moduleId);
         const onlineVersion = module.version;
 
         if (localVersion === onlineVersion) {
-            const cachedScript = getModuleScript(menuIndex, index);
+            const cachedScript = getModuleScript(menuId, moduleId);
             if (cachedScript) {
                 try {
                     eval(cachedScript);
@@ -83,24 +108,25 @@
             onload: function(response) {
                 try {
                     const scriptContent = response.responseText;
-                    saveModuleScript(menuIndex, index, scriptContent);
+                    saveModuleScript(menuId, moduleId, scriptContent);
                     eval(scriptContent);
-                    setModuleLocalVersion(menuIndex, index, module.version);
-                    updateControlPanel();
+                    setModuleLocalVersion(menuId, moduleId, module.version);
+                    updateVersionDisplay(menuId, moduleId);
                 } catch (e) {}
             },
             onerror: function() {}
         });
     }
 
-    function getModuleByIndex(menuIndex, index) {
-        return modulesConfig.menu[menuIndex].modules[index];
+    function getModuleById(menuId, moduleId) {
+        const menu = modulesConfig.find(menu => menu.id === menuId);
+        return menu.modules.find(module => module.id === moduleId);
     }
 
-    function saveModuleScript(menuIndex, index, scriptContent) {
+    function saveModuleScript(menuId, moduleId, scriptContent) {
         const scriptParts = splitScriptIntoParts(scriptContent);
         scriptParts.forEach((part, partIndex) => {
-            GM_setValue(`module_${menuIndex}_${index}_script_part_${partIndex}`, part);
+            GM_setValue(`module_${menuId}_${moduleId}_script_part_${partIndex}`, part);
         });
     }
 
@@ -113,11 +139,11 @@
         return parts;
     }
 
-    function getModuleScript(menuIndex, index) {
+    function getModuleScript(menuId, moduleId) {
         let scriptContent = '';
         let partIndex = 0;
         while (true) {
-            const part = GM_getValue(`module_${menuIndex}_${index}_script_part_${partIndex}`);
+            const part = GM_getValue(`module_${menuId}_${moduleId}_script_part_${partIndex}`);
             if (!part) break;
             scriptContent += part;
             partIndex++;
@@ -140,7 +166,7 @@
         panel.style.fontFamily = 'Arial, sans-serif';
 
         const title = document.createElement('h4');
-        title.innerText = 'Make Azusa Great Again';
+        title.innerText = 'Make Azusa Great Again !';
         title.style.margin = '0 0 10px 0';
         title.style.textAlign = 'center';
         panel.appendChild(title);
@@ -156,15 +182,31 @@
         hideText.addEventListener('click', () => {
             panel.style.display = 'none';
             floatingButton.style.display = 'block';
-            GM_setValue('panel_hidden', true); // Save the hidden state
+            GM_setValue('panel_hidden', true);
         });
         panel.appendChild(hideText);
+
+        const clearStorageButton = document.createElement('span');
+        clearStorageButton.innerHTML = '[清空数据]';
+        clearStorageButton.style.position = 'absolute';
+        clearStorageButton.style.top = '10px';
+        clearStorageButton.style.left = '10px';
+        clearStorageButton.style.color = 'grey';
+        clearStorageButton.style.cursor = 'pointer';
+        clearStorageButton.style.fontSize = '14px';
+        clearStorageButton.addEventListener('click', () => {
+            if (confirm('确定要清空MAGA本地存储的数据吗？\n清空后会自动刷新网页\n建议只在有问题的情况下清空本地数据')) {
+                clearLocalStorage();
+                location.reload();
+            }
+        });
+        panel.appendChild(clearStorageButton);
 
         const panelContent = document.createElement('div');
         panelContent.id = 'panel-content';
         panel.appendChild(panelContent);
 
-        modulesConfig.menu.forEach((menu, menuIndex) => {
+        modulesConfig.forEach((menu) => {
             const menuTitle = document.createElement('h5');
             menuTitle.innerText = menu.name;
             menuTitle.style.fontSize = '18px';
@@ -172,7 +214,7 @@
             menuTitle.style.textAlign = 'center';
             panelContent.appendChild(menuTitle);
 
-            menu.modules.forEach((module, index) => {
+            menu.modules.forEach((module) => {
                 const checkboxContainer = document.createElement('div');
                 checkboxContainer.style.marginBottom = '5px';
                 checkboxContainer.style.fontSize = '14px';
@@ -186,11 +228,8 @@
                 leftContainer.style.alignItems = 'center';
                 leftContainer.style.whiteSpace = 'nowrap';
 
-                const checkbox = document.createElement('input');
-                checkbox.type = 'checkbox';
-                checkbox.id = `module-${menuIndex}-${index}`;
-                checkbox.checked = getModuleState(menuIndex, index);
-                checkbox.addEventListener('change', (event) => toggleModuleState(menuIndex, index, event.target));
+                const checkbox = createSwitch(menu.id, module.id);
+                checkbox.checked = getModuleState(menu.id, module.id);
 
                 const label = document.createElement('label');
                 label.innerText = module.name;
@@ -214,11 +253,13 @@
                 versionContainer.style.whiteSpace = 'nowrap';
 
                 const versionDisplay = document.createElement('span');
-                const localVersion = getModuleLocalVersion(menuIndex, index);
+                versionDisplay.id = `version-display-${menu.id}-${module.id}`;
+                const localVersion = getModuleLocalVersion(menu.id, module.id);
                 const onlineVersion = module.version;
                 versionDisplay.innerHTML = `本地版本: ${localVersion} | 在线版本: ${onlineVersion}`;
 
                 const updateButton = document.createElement('button');
+                updateButton.id = `update-button-${menu.id}-${module.id}`;
                 updateButton.innerHTML = '升级';
                 updateButton.style.marginLeft = '10px';
                 updateButton.style.padding = '5px 10px';
@@ -231,7 +272,7 @@
                 updateButton.disabled = (localVersion === onlineVersion);
                 updateButton.style.backgroundColor = (localVersion === onlineVersion) ? '#bbb' : '#333';
 
-                updateButton.addEventListener('click', () => updateModule(menuIndex, index));
+                updateButton.addEventListener('click', () => updateModule(menu.id, module.id));
 
                 versionContainer.appendChild(versionDisplay);
                 versionContainer.appendChild(updateButton);
@@ -253,18 +294,17 @@
         extraEmptyLine.style.height = '10px';
         panelContent.appendChild(extraEmptyLine);
 
-        const scriptVersion = GM_info.script.version;
-
         const authorInfo = document.createElement('div');
         authorInfo.style.position = 'absolute';
         authorInfo.style.bottom = '10px';
         authorInfo.style.left = '10px';
         authorInfo.style.fontSize = '12px';
         authorInfo.style.textAlign = 'left';
-        authorInfo.textContent = `作者: ERST`;
+        authorInfo.textContent = '作者: ERST';
         panel.appendChild(authorInfo);
 
         const versionInfo = document.createElement('div');
+        versionInfo.id = 'script-version-info';
         versionInfo.style.position = 'absolute';
         versionInfo.style.bottom = '10px';
         versionInfo.style.right = '10px';
@@ -272,6 +312,20 @@
         versionInfo.style.textAlign = 'right';
         versionInfo.textContent = `版本: ${scriptVersion}`;
         panel.appendChild(versionInfo);
+
+        const newVersionAlert = document.createElement('div');
+        newVersionAlert.id = 'new-version-alert';
+        newVersionAlert.style.position = 'absolute';
+        newVersionAlert.style.bottom = '10px';
+        newVersionAlert.style.left = '50%';
+        newVersionAlert.style.transform = 'translateX(-50%)';
+        newVersionAlert.style.fontSize = '12px';
+        newVersionAlert.style.textAlign = 'center';
+        newVersionAlert.style.color = 'red';
+        newVersionAlert.style.display = 'none';
+        newVersionAlert.style.whiteSpace = 'nowrap';
+        newVersionAlert.innerText = '(新版本可用)';
+        panel.appendChild(newVersionAlert);
 
         const floatingButton = document.createElement('div');
         floatingButton.id = 'floating-button';
@@ -289,75 +343,165 @@
         floatingButton.addEventListener('click', () => {
             panel.style.display = 'block';
             floatingButton.style.display = 'none';
-            GM_setValue('panel_hidden', false); // Save the hidden state
+            GM_setValue('panel_hidden', false);
         });
         document.body.appendChild(floatingButton);
 
-        // Restore the hidden state
         if (GM_getValue('panel_hidden', false)) {
             panel.style.display = 'none';
             floatingButton.style.display = 'block';
         }
     }
 
-    function getModuleState(menuIndex, index) {
-        return GM_getValue(`module_${menuIndex}_${index}_enabled`, false);
+    function createSwitch(menuId, moduleId) {
+        const switchLabel = document.createElement('label');
+        switchLabel.classList.add('switch');
+
+        const input = document.createElement('input');
+        input.type = 'checkbox';
+        input.id = `module-${menuId}-${moduleId}`;
+        input.checked = getModuleState(menuId, moduleId);
+        input.addEventListener('change', (event) => toggleModuleState(menuId, moduleId, event.target));
+
+        const slider = document.createElement('span');
+        slider.classList.add('slider');
+
+        switchLabel.appendChild(input);
+        switchLabel.appendChild(slider);
+
+        return switchLabel;
     }
 
-    function getModuleLocalVersion(menuIndex, index) {
-        return GM_getValue(`module_${menuIndex}_${index}_version`, '未安装');
+    function getModuleState(menuId, moduleId) {
+        return GM_getValue(`module_${menuId}_${moduleId}_enabled`, false);
     }
 
-    function setModuleState(menuIndex, index, state) {
-        GM_setValue(`module_${menuIndex}_${index}_enabled`, state);
+    function getModuleLocalVersion(menuId, moduleId) {
+        return GM_getValue(`module_${menuId}_${moduleId}_version`, '未安装');
     }
 
-    function setModuleLocalVersion(menuIndex, index, version) {
-        GM_setValue(`module_${menuIndex}_${index}_version`, version);
+    function setModuleState(menuId, moduleId, state) {
+        GM_setValue(`module_${menuId}_${moduleId}_enabled`, state);
     }
 
-    function toggleModuleState(menuIndex, index, checkbox) {
-        const currentState = getModuleState(menuIndex, index);
+    function setModuleLocalVersion(menuId, moduleId, version) {
+        GM_setValue(`module_${menuId}_${moduleId}_version`, version);
+    }
+
+    function updateVersionDisplay(menuId, moduleId) {
+        const versionDisplay = document.getElementById(`version-display-${menuId}-${moduleId}`);
+        const localVersion = getModuleLocalVersion(menuId, moduleId);
+        const onlineVersion = modulesConfig.find(menu => menu.id === menuId).modules.find(module => module.id === moduleId).version;
+        versionDisplay.innerHTML = `本地版本: ${localVersion} | 在线版本: ${onlineVersion}`;
+
+        const updateButton = document.getElementById(`update-button-${menuId}-${moduleId}`);
+        const buttonDisabled = (localVersion === onlineVersion);
+        updateButton.disabled = buttonDisabled;
+        updateButton.style.backgroundColor = buttonDisabled ? '#bbb' : '#333';
+    }
+
+    function toggleModuleState(menuId, moduleId, checkbox) {
+        const currentState = getModuleState(menuId, moduleId);
         const newState = !currentState;
-        setModuleState(menuIndex, index, newState);
+        setModuleState(menuId, moduleId, newState);
         checkbox.checked = newState;
-        updateControlPanel();
         if (newState) {
-            loadAndExecuteModule(menuIndex, index);
+            loadAndExecuteModule(menuId, moduleId);
+            const module = getModuleById(menuId, moduleId);
+            setModuleLocalVersion(menuId, moduleId, module.version);
         }
+        updateVersionDisplay(menuId, moduleId);
     }
 
     function updateControlPanel() {
-        modulesConfig.menu.forEach((menu, menuIndex) => {
-            menu.modules.forEach((module, index) => {
-                const checkbox = document.querySelector(`#module-${menuIndex}-${index}`);
+        modulesConfig.forEach((menu) => {
+            menu.modules.forEach((module) => {
+                const checkbox = document.querySelector(`#module-${menu.id}-${module.id}`);
                 const versionDisplay = checkbox ? checkbox.parentElement.nextElementSibling.querySelector('span') : null;
                 if (versionDisplay) {
-                    const localVersion = getModuleLocalVersion(menuIndex, index);
+                    const localVersion = getModuleLocalVersion(menu.id, module.id);
                     const onlineVersion = module.version;
                     versionDisplay.innerHTML = `本地版本: ${localVersion} | 在线版本: ${onlineVersion}`;
+                    updateVersionDisplay(menu.id, module.id);
                 }
             });
         });
     }
 
-    function updateModule(menuIndex, index) {
-        const module = getModuleByIndex(menuIndex, index);
+    function updateModule(menuId, moduleId) {
+        const module = getModuleById(menuId, moduleId);
         GM_xmlhttpRequest({
             method: 'GET',
             url: module.url,
             onload: function(response) {
                 try {
                     const scriptContent = response.responseText;
-                    saveModuleScript(menuIndex, index, scriptContent);
+                    saveModuleScript(menuId, moduleId, scriptContent);
                     eval(scriptContent);
-                    setModuleLocalVersion(menuIndex, index, module.version);
-                    updateControlPanel();
+                    setModuleLocalVersion(menuId, moduleId, module.version);
+                    updateVersionDisplay(menuId, moduleId);
                 } catch (e) {}
             },
             onerror: function() {}
         });
     }
+
+    function clearLocalStorage() {
+        GM_listValues().forEach((key) => {
+            GM_deleteValue(key);
+        });
+    }
+
+    GM_addStyle(`
+        .switch {
+            position: relative;
+            display: inline-block;
+            width: 34px;
+            height: 20px;
+        }
+
+        .switch input {
+            opacity: 0;
+            width: 0;
+            height: 0;
+        }
+
+        .slider {
+            position: absolute;
+            cursor: pointer;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            background-color: #ccc;
+            transition: .4s;
+            border-radius: 20px;
+        }
+
+        .slider:before {
+            position: absolute;
+            content: "";
+            height: 12px;
+            width: 12px;
+            left: 4px;
+            bottom: 4px;
+            background-color: white;
+            transition: .4s;
+            border-radius: 50%;
+        }
+
+        input:checked + .slider {
+            background-color: #2196F3;
+        }
+
+        input:checked + .slider:before {
+            transform: translateX(14px);
+        }
+
+        #new-version-alert {
+            display: none;
+        }
+    `);
 
     fetchModulesConfig();
 
