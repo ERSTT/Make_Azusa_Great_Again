d91ca14a8a08b4446ddc743ec2a580561f5d2a7d
ERST <158469810+ERSTT@users.noreply.github.com>
2025-01-21 14:49:41 +0800

    Update Azusa_expand_the_page_width.user.js

--- JavaScript/Azusa_expand_the_page_width.user.js
+++ JavaScript/Azusa_expand_the_page_width.user.js
@@ -1,18 +1,30 @@
 // ==UserScript==
-// @name        Azusa 扩宽页面
+// @name        Azusa 自适应宽度页面
 // @namespace   https://greasyfork.org/users/1396048-moeruotaku
-// @version     2024.12.1.1264
+// @version     2024.12.1.1266
 // @author      moeruotaku
 // @match       https://azusa.wiki/*
 // @match       https://zimiao.icu/*
-// @icon         https://azusa.wiki/favicon.ico
+// @icon        https://azusa.wiki/favicon.ico
 // @license     MIT
 // ==/UserScript==
 
 (function () {
     'use strict';
 
-    let mainouter = document.getElementsByClassName('mainouter')[0];
-    let main = document.getElementsByClassName('main')[0];
-    if (mainouter && main && mainouter.parentNode.offsetWidth - mainouter.clientWidth > 600) main.width = (parseInt(main.width, 10) + 400).toString();
+    let mainouter = document.querySelector('.mainouter');
+    let main = document.querySelector('.main');
+
+    if (mainouter && main) {
+        const adjustWidth = () => {
+            let parentWidth = mainouter.parentNode.offsetWidth;
+            main.style.width = `${parentWidth * 0.9}px`;
+            main.style.maxWidth = '999999px';
+            main.style.margin = '0 auto';
+        };
+
+        adjustWidth();
+
+        window.addEventListener('resize', adjustWidth);
+    }
 })();
